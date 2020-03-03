0feba9ec4d752ac213526082419f61888af68adf
Павел Глухов <p.glukhov@planfact.io>
2020-03-03 12:29:12 +0300

    init

--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -19,10 +19,10 @@ namespace Core
             AccountCreationRequest dataForPost = new AccountCreationRequest { Title = "console request", AccountBank = null, AccountBik = null, AccountType = "Cash", AccountAcct = null, Active = true, CompanyId = 41104, CurrencyCode = "USD", Description = null, LongTitle = "", Remainder = 0, RemainderDate = "2020-03-02" };
             OperationModificationRequest dataForPut = new OperationModificationRequest { AccountId = 106080, Comment = "", IsCommitted = false, OperationDate = "2020-02-25", Items = items };
             string apiKey = "DoSxssgpo1jcSfGVB2nS4hTlgP_QfXdx42eOnhnIZcJHc5SDo9i6OuMPB46zVduOKoE65ajOivvjM02RpnZ6HjbRx4v4a4Om3E9lGY3AHL6RTkCEsud_3sm3HHvE2WamNl2gfYD7hnQ2O__3QwVMdUUsae5AkXPMWmSVppCQvvxinVrqnjTmJ2hz_GauwgaqO_1jrR24RTyzOCC0BRfbskbdIj6md5n4Ds6wS8rHfR-I525Tnp6G7hdaoEwPkfCivsvHfhQA_TJHb9_Upv2wd02pBZOF7dtOxThEs1PNQEyOnbvlopRAvwuEIkLknMpaoJPz01C8eic2zsTno-xmw9u5L_0N_zHuRwBp0KubuakiaCLBKavDvcfE8EsWF7eha6y7Bj467B9BiH-ut-h6_TsT8XhaXCYXr__SdrxPs2N2ElzJzN5prZqK5Tfd6HX9hXQMYVEC8vVZZsyaYGp56In4UCte0d8c08VukZRVDtCHWpOCMryaUfGk3OsbUX3-YTIfctROJBRhDs8r1cI930wbId3HgpAYKMrgi0b-YmJHNO4qv3ZaVATO8hKseUb6gIgmZQ";
-            //request.Delete(apiKey, 16367698, client);
-            //request.Get(apiKey, client);
-            //Console.WriteLine(request.Post(apiKey, dataForPost, client).GetAwaiter().GetResult());
-            //Console.WriteLine(request.Put(apiKey,16367698, dataForPut, client).GetAwaiter().GetResult());
+            request.Delete(apiKey, 16367698, client);
+            request.Get(apiKey, client);
+            Console.WriteLine(request.Post(apiKey, dataForPost, client).GetAwaiter().GetResult());
+            Console.WriteLine(request.Put(apiKey,16367698, dataForPut, client).GetAwaiter().GetResult());
         }
 
         static HttpClient CreateClient()
