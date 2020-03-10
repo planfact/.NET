3e6075cc918b70cf1e0c7e83242f1c2db1dedac4
Павел Глухов <p.glukhov@planfact.io>
2020-03-10 10:33:12 +0300

    init

--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -11,18 +11,30 @@ namespace Core
     {
         static void Main(string[] args)
         {
+            //Создаём объект класса Response для того, чтобы записать туда ответ.
             Response<AccountModel> response = new Response<AccountModel>();
+            //Создаём объект класса HttpClient.
             HttpClient client = CreateClient();
+            //Создаём объект класса RequestExample для того, чтобы через него вызывать методы, которые выполняют запросы
             RequestExample request = new RequestExample();
+            //Создаём список частей операций.
             List<OperationItemRequest> items = new List<OperationItemRequest>();
+            //Заполняем список частей операции
             items.Add(new OperationItemRequest { CalculationDate = "2020-02-25", ContrAgentId = null, Value = 123, IsCalculationCommitted = false, OperationCategoryId = null, ProjectId = null });
-            AccountCreationRequest dataForPost = new AccountCreationRequest { Title = "console request", AccountBank = null, AccountBik = null, AccountType = "Cash", AccountAcct = null, Active = true, CompanyId = 41104, CurrencyCode = "USD", Description = null, LongTitle = "", Remainder = 0, RemainderDate = "2020-03-02" };
+            //Создаём объект класса AccountCreationRequest и заполняем его данными для добавления счёта.
+            AccountCreationRequest dataForPost = new AccountCreationRequest { Title = "test", AccountBank = null, AccountBik = null, AccountType = "Cash", AccountAcct = null, Active = true, CompanyId = 41104, CurrencyCode = "USD", Description = null, LongTitle = "", Remainder = 0, RemainderDate = "2020-03-02" };
+            //Создаём объект класса OperationModificationRequest и заполняем его данными для изменнения операции.
             OperationModificationRequest dataForPut = new OperationModificationRequest { AccountId = 106080, Comment = "", IsCommitted = false, OperationDate = "2020-02-25", Items = items };
-            string apiKey = "DoSxssgpo1jcSfGVB2nS4hTlgP_QfXdx42eOnhnIZcJHc5SDo9i6OuMPB46zVduOKoE65ajOivvjM02RpnZ6HjbRx4v4a4Om3E9lGY3AHL6RTkCEsud_3sm3HHvE2WamNl2gfYD7hnQ2O__3QwVMdUUsae5AkXPMWmSVppCQvvxinVrqnjTmJ2hz_GauwgaqO_1jrR24RTyzOCC0BRfbskbdIj6md5n4Ds6wS8rHfR-I525Tnp6G7hdaoEwPkfCivsvHfhQA_TJHb9_Upv2wd02pBZOF7dtOxThEs1PNQEyOnbvlopRAvwuEIkLknMpaoJPz01C8eic2zsTno-xmw9u5L_0N_zHuRwBp0KubuakiaCLBKavDvcfE8EsWF7eha6y7Bj467B9BiH-ut-h6_TsT8XhaXCYXr__SdrxPs2N2ElzJzN5prZqK5Tfd6HX9hXQMYVEC8vVZZsyaYGp56In4UCte0d8c08VukZRVDtCHWpOCMryaUfGk3OsbUX3-YTIfctROJBRhDs8r1cI930wbId3HgpAYKMrgi0b-YmJHNO4qv3ZaVATO8hKseUb6gIgmZQ";
+            //Создаём переменую для хранения api-ключа.
+            string apiKey = "Ваш api-ключ";
+            //Вызываем метод Delete для удаления операции и передаём ему в виде параметров api-ключ, id операции и объект класса HttpClient.
             request.Delete(apiKey, 16367698, client);
+            //Вызываем метод Get для получения списка счетов и передаём ему в виде параметров api-ключ и объект класса HttpClient. 
             request.Get(apiKey, client);
+            //Выводим в консоль результат метода Post для добавления счёта. В параметрах этого метода передаём api-ключ, данные счёта, который мы хотим добавить, объект класса HttpClient.
             Console.WriteLine(request.Post(apiKey, dataForPost, client).GetAwaiter().GetResult());
-            Console.WriteLine(request.Put(apiKey,16367698, dataForPut, client).GetAwaiter().GetResult());
+            //Выводим в консоль результат метода Put для изменения операции. В параметрах этого метода передаём api-ключ, id операции, изменённые данные операции, объект класса HttpClient.
+            Console.WriteLine(request.Put(apiKey, 16367698, dataForPut, client).GetAwaiter().GetResult());
         }
 
         static HttpClient CreateClient()
